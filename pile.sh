7c267a4be0b485e06652a416104f9db2a4e5d1f5
Kevin Chapman <kchapmann@gmail.com>
2014-03-09 00:28:44 -0500

    base version of pile.sh.

--- pile.sh
+++ pile.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# the pile version 0.1
-# 02/27/2014
+# the pile version 0.0
+# aggregate all TODO's in a directory.
+#  -currently only works for one (current) dir, this will be expanded
+#  -currently only works for TODO's commented with a 1 character comment
 
-CURRENT_DIR=$(pwd)
-FILES=$("find $CURRENT_DIR")
+#TODO: only match TODO's that start with a(ny) comment
+#TODO: print filename before group of TODO's from that file
+#TODO: come up with a way to handle TODO's that span more than one line
+#TODO: think about adding a script to check for necessary tools
 
-for X in $FILES
+
+FILES=(*)
+
+for X in "${FILES[@]}"
 do
-	echo "$X"
+	echo "`pwd`/$X" >> .output
+	sed -n "s/^.\(TODO:.*$\)/  \1/p" "$X" >> .output
+	#TODO: color formatting of output. might have to format later
 done
+
+cat .output
+rm .output
